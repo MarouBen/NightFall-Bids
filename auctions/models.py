7c26aa13d3beb0725334ef13c58e74362a2de8dc
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-02-27 14:25:04 +0100

    bids model
    a model that contain bids and make a relationship between Listings and users

--- auctions/models.py
+++ auctions/models.py
@@ -3,16 +3,23 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """" Users table is created by default by the class AbstractUser """
+    """"Users table is created by default by the class AbstractUser"""
     pass
 
 class Listings(models.model):
-    """" The table that will hold the listing name, details text, starting bid,
-    date when it was posted and finally an image """
+    """"The table that will hold the listing name, details text, starting bid,
+    date when it was posted and finally an image"""
     
     # The id is created automatically
     name = models.CharField(max_length=64)
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
-    startingBid = models.DecimalField(max_digits=10,decimal_places=2)
+    startingBid = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to="Images/Listings/")
+    
+class Bids(models.model):
+    """"Bids table will contain all the bids that are placed on a certain listing by a certain user"""
+    
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE)
