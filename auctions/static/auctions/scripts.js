e234593fa2b13885012696a29f727714098623e4
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-03-04 16:10:12 +0100

    can list with category
     fixed the none problem

--- auctions/static/auctions/scripts.js
+++ auctions/static/auctions/scripts.js
@@ -1,13 +1,13 @@
-// Get the dropdown button element
-const dropdownButton = document.querySelector('.dropdown-toggle');
+// // Get the dropdown button element
+// const dropdownButton = document.querySelector('.dropdown-toggle');
   
-// Get the dropdown menu items
-const dropdownItems = document.querySelectorAll('.dropdown-item');
+// // Get the dropdown menu items
+// const dropdownItems = document.querySelectorAll('.dropdown-item');
 
-// Loop through the dropdown menu items and add a click event listener to each one
-dropdownItems.forEach(item => {
-  item.addEventListener('click', () => {
-    // Update the title of the dropdown button with the selected option
-    dropdownButton.textContent = item.textContent;
-  });
-});
+// // Loop through the dropdown menu items and add a click event listener to each one
+// dropdownItems.forEach(item => {
+//   item.addEventListener('click', () => {
+//     // Update the title of the dropdown button with the selected option
+//     dropdownButton.textContent = item.textContent;
+//   });
+// });
