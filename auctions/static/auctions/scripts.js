ffb89f79994079a04a4d0bd650205441d4a74049
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-03-07 15:05:33 +0100

    added a collapsing search bar

--- auctions/static/auctions/scripts.js
+++ auctions/static/auctions/scripts.js
@@ -1,4 +1,15 @@
+const form = document.getElementById("search_form");
+const input = document.getElementById("search_input");
+
+input.addEventListener('keyup', function(event) {
+if (event.keyCode === 13) {
+    event.preventDefault();
+    form.submit();
+    }
+});
+
 function scrollToTarget() {
-    document.getElementById("scrollTarget").scrollIntoView({ behavior: 'smooth' });
-  }
+  document.getElementById("scrollTarget").scrollIntoView({ behavior: 'smooth' });
+}
+
   
