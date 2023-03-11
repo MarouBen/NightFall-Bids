67cae0dce33553faae7732d594ed4306f48ec64e
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-03-11 12:26:56 +0100

    added requirement

--- auctions/views.py
+++ auctions/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 from django.views.decorators.cache import never_cache
 
 from .models import User,Listing,ListingImage,Bid,Comment
