7e19354748d884f523e2d8b5df2560d698e22447
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-07-02 13:59:45 +0100

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -14,7 +14,7 @@ LOGIN_URL = 'login'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nightfallbids-38f8ad1a82fe.herokuapp.com']
 
