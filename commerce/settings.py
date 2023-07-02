1c2ec6a4ecb3aae262dd58488f0237e3e3950e15
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-07-02 13:44:13 +0100

    dfsf

--- commerce/settings.py
+++ commerce/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nightfallbids-38f8ad1a82fe.herokuapp.com']
 
 
 # Application definition
